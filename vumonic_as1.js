1d2b81cafa5d1a80bd73453886e5750daac62ffb
architagrawal <32601591+architagrawal@users.noreply.github.com>
2022-05-03 16:43:12 +0530

    Add files via upload

--- vumonic_as1.js
+++ vumonic_as1.js
@@ -1,29 +1,36 @@
 const Imap = require('imap');
 const {simpleParser} = require('mailparser');
-
+var {convert} = require('html-to-text');
+var jsonfile = require('jsonfile')
+let jsonVariable = []
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
-/*const imapConfig = {
-  user: '',
-  password: '',
+const imapConfig = {
+  user: 'architagrawal000@gmail.com',
+  password: 'ibqhthdctxwvwodc',
   host: 'imap.gmail.com',
   port: 993,
-  tls: true,
+  tls: true
 };
-*/
+
 const getEmails = () => {
   try {
     const imap = new Imap(imapConfig);
     imap.once('ready', () => {
       imap.openBox('INBOX', false, () => {
-        imap.search(['UNSEEN', ['SINCE','April 30, 2022']], (err, results) => {
+        imap.search(['ALL', ['SINCE','May 03, 2022']], (err, results) => {
           const f = imap.fetch(results, {bodies: ''});
           f.on('message', msg => {
             msg.on('body', stream => {
               simpleParser(stream, async (err, parsed) => {
-                const {from, subject, textAsHtml, text} = parsed;
-                console.log(parsed);
+                const {from, to,subject, date, textAsHtml, html,text} = parsed;
+                let frm = from.value[0].address
+                let t = to.value[0].address
+                let txt=convert(text);
+                let dat = {"from":frm,"to":t,"subject":subject,"date": date, "text":txt,}
+jsonVariable.push(dat)
               });
+
             });
             msg.once('attributes', attrs => {
               const {uid} = attrs;
@@ -36,10 +43,72 @@ const getEmails = () => {
             return Promise.reject(ex);
           });
           f.once('end', () => {
+
+
             console.log('Done fetching all messages!');
             imap.end();
           });
         });
+
+      });
+    });
+
+    imap.once('error', err => {
+      console.log(err);
+    });
+
+    imap.once('end', () => {
+      jsonfile.writeFile('ArchitAssigned.json', jsonVariable, {spaces:2}, function(err){
+        console.log(err);})
+      console.log('Connection ended');
+    });
+
+    imap.connect();
+  } catch (ex) {
+    console.log('an error occurred');
+  }
+};
+
+getEmails();
+
+//part 2
+const recentEmails = () => {
+  try {
+    const imap = new Imap(imapConfig);
+    imap.once('ready', () => {
+      imap.openBox('INBOX', false, () => {
+        imap.search(['RECENT', ['SINCE','May 03, 2021']], (err, results) => {
+          const f = imap.fetch(results, {bodies: ''});
+          f.on('message', msg => {
+            msg.on('body', stream => {
+              simpleParser(stream, async (err, parsed) => {
+                const {from, to,subject, date, textAsHtml, html,text} = parsed;
+                let frm = from.value[0].address
+                let t = to.value[0].address
+                let txt=convert(text);
+                let dat = {"from":frm,"to":t,"subject":subject,"date": date, "text":txt,}
+console.log(dat)
+              });
+
+            });
+            msg.once('attributes', attrs => {
+              const {uid} = attrs;
+              imap.addFlags(uid, ['\\Seen'], () => {
+                console.log('Marked as read!');
+              });
+            });
+          });
+          f.once('error', ex => {
+            return Promise.reject(ex);
+          });
+          f.once('end', () => {
+
+
+            console.log('Done fetching all messages!');
+            imap.end();
+          });
+        });
+
       });
     });
 
@@ -56,5 +125,10 @@ const getEmails = () => {
     console.log('an error occurred');
   }
 };
+// recentEmails();
 
-getEmails();
+/*
+Ideally , our code should listen for MAIL connection event. it pings when there is any update.
+ But as I am not able to implement that, I used a easy way. Uncomment line 128 recentEmails(); and comment line 72 getEmails();
+ for part 2 of assignment.
+  */
